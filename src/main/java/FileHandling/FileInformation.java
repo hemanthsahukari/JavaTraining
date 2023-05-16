4370f137f0870052bc2c8f6bbcc4879fa9a5a8e1
hemanthsahukari <hemanthsahukari59@gmail.com>
2023-05-17 00:54:43 +0530

    Initial commit

---
+++ src/main/java/FileHandling/FileInformation.java
@@ -0,0 +1,17 @@
+package FileHandling;
+import java.io.File;
+
+public class FileInformation {
+    public static void main(String[] args) {
+        File myObj = new File("File.txt");
+        if(myObj.exists()){
+            System.out.println("File name: "+ myObj.getName());
+            System.out.println("Absloute path: "+ myObj.getAbsolutePath());
+            System.out.println("Writeable: "+ myObj.canWrite());
+            System.out.println("Readable: "+myObj.canRead());
+            System.out.println("File size in bytes "+myObj.length());
+        }else{
+            System.out.println("The file does not exist.");
+        }
+    }
+}
