53ac999e209b811c67506d6c42579d96eed4f223
hemanthsahukari <hemanthsahukari59@gmail.com>
2023-05-22 15:23:01 +0530

    git commit

--- src/main/java/CollectionFramework/Example.java
+++ src/main/java/CollectionFramework/Example.java
@@ -3,12 +3,13 @@ package CollectionFramework;
 
 
 //sort array list....
+import java.util.Iterator;
 import java.util.List;
 import java.util.Collections;
 import java.util.ArrayList;
 public class Example  {
-    public static void main(String args[]) {
-        List<String> list1 = new ArrayList<String>();
+    public static void main(String[] args) {
+        List<String> list1 = new ArrayList<>();
         list1.add("hemanth");
         list1.add("Jaya");
         list1.add("shiva");
@@ -36,3 +37,42 @@ public class Example  {
     }
 }
 
+
+
+//User-defined class objects in Java ArrayList
+class ArrayList1{
+    public static void main(String args[]){
+        class Student{
+            int rollno;
+            String name;
+            int age;
+            Student(int rollno,String name,int age){
+                this.rollno=rollno;
+                this.name=name;
+                this.age=age;
+            }
+        }
+        //Creating user-defined class objects
+        Student s1=new Student(101,"Levi",23);
+        Student s2=new Student(102,"eren",21);
+        Student s3=new Student(103,"erwin",25);
+        //creating arraylist
+        ArrayList<Student> al= new ArrayList<>();
+        al.add(s1);//adding Student class object
+        al.add(s2);
+        al.add(s3);
+
+
+        /*.........to remove elements....
+         al.remove("s2");
+         al.remove(0);.........*/
+
+        //Getting Iterator
+        Iterator<Student> itr=al.iterator();
+        //traversing elements of ArrayList object
+        while(itr.hasNext()){
+            Student st=(Student)itr.next();
+            System.out.println(st.rollno+" "+st.name+" "+st.age);
+        }
+    }
+}
